bd434bcac28fca33f9bf705a16f46520538114d5
chenjing185 <adf13148320@163.com>
2022-10-07 15:58:16 +0800

    检查登录状态

--- src/main/java/com/nowcoder/community/util/CommunityUtil.java
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -1,8 +1,11 @@
 package com.nowcoder.community.util;
 
 import org.apache.commons.lang3.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.util.DigestUtils;
 
+import java.util.Map;
 import java.util.UUID;
 
 public class CommunityUtil {
@@ -22,4 +25,27 @@ public class CommunityUtil {
         return DigestUtils.md5DigestAsHex(key.getBytes());
     }
 
+    public static String getJSONString(int code, String msg, Map<String, Object> map) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("code", code);
+            json.put("msg", msg);
+            if(map != null){
+                for(String key : map.keySet()){
+                    json.put(key, map.get(key));
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json.toString();
+    }
+
+    public static String getJSONString(int code, String msg){
+        return getJSONString(code, msg, null);
+    }
+
+    public static String getJSONString(int code){
+        return getJSONString(code, null, null);
+    }
 }
