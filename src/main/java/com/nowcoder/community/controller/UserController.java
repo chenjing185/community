a98ded78ae8d02fc0c0deed56f86b5aee8119ea2
chenjing185 <adf13148320@163.com>
2022-11-19 21:31:54 +0800

    关注、取消关注

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.FollowService;
 import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.apache.commons.lang3.StringUtils;
@@ -18,11 +20,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 /**
@@ -32,7 +34,7 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements CommunityConstant {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Value("${community.path.upload}")
@@ -53,68 +55,71 @@ public class UserController {
     @Autowired
     private LikeService likeService;
 
-    @LoginRequired
-    @RequestMapping(path = "/setting",method = RequestMethod.GET)
-    public String getSettingPage(){
-        return "/site/setting";
+    @Autowired
+    private FollowService followService;
 
+    @LoginRequired
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage() {
+        return "/site/setting";
     }
 
     @LoginRequired
-    @RequestMapping(path = "/upload",method = RequestMethod.POST)
-    public String uploadHeader(MultipartFile headerImage, Model model){
-        if (headerImage == null){
-            model.addAttribute("error","您还没有选择图片");
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        if (headerImage == null) {
+            model.addAttribute("error", "您还没有选择图片!");
             return "/site/setting";
         }
 
-        String filename = headerImage.getOriginalFilename();
-        String suffix = filename.substring(filename.lastIndexOf("."));
-        if (StringUtils.isBlank(suffix)){
-            model.addAttribute("error","文件的格式不正确!");
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if (StringUtils.isBlank(suffix)) {
+            model.addAttribute("error", "文件的格式不正确!");
             return "/site/setting";
         }
 
-        //生成随机文件名
-        filename = CommunityUtil.generateUUID() + suffix;
-        File dest = new File(uploadPath + "/" + filename);
+        // 生成随机文件名
+        fileName = CommunityUtil.generateUUID() + suffix;
+        // 确定文件存放的路径
+        File dest = new File(uploadPath + "/" + fileName);
         try {
+            // 存储文件
             headerImage.transferTo(dest);
         } catch (IOException e) {
-            logger.error("上传文件失败:" + e.getMessage());
-            throw new RuntimeException("上传文件失败,服务器发生异常!",e);
+            logger.error("上传文件失败: " + e.getMessage());
+            throw new RuntimeException("上传文件失败,服务器发生异常!", e);
         }
 
-        //更新当前用户的头像的路径
+        // 更新当前用户的头像的路径(web访问路径)
+        // http://localhost:8080/community/user/header/xxx.png
         User user = hostHolder.getUser();
-        String headerUrl = domain + contextPath + "/user/header/" + filename;
-        userService.updateHeader(user.getId(),headerUrl);
+        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(), headerUrl);
 
         return "redirect:/index";
-
     }
 
-    @RequestMapping(path = "/header/{fileName}",method = RequestMethod.GET)
-    public void getHeader(@PathVariable("fileName") String fileName, HttpServletResponse response){
-        //服务器存放路径
+    @RequestMapping(path = "/header/{fileName}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        // 服务器存放路径
         fileName = uploadPath + "/" + fileName;
-        //文件后缀
+        // 文件后缀
         String suffix = fileName.substring(fileName.lastIndexOf("."));
-        //响应图片
+        // 响应图片
         response.setContentType("image/" + suffix);
         try (
-            FileInputStream fis = new FileInputStream(fileName);
-            ServletOutputStream os = response.getOutputStream();
-        ){
+                FileInputStream fis = new FileInputStream(fileName);
+                OutputStream os = response.getOutputStream();
+        ) {
             byte[] buffer = new byte[1024];
             int b = 0;
-            while ((b = fis.read(buffer)) != -1){
-                os.write(buffer,0,b);
+            while ((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
             }
         } catch (IOException e) {
-            logger.error("读取头像失败:" + e.getMessage());
+            logger.error("读取头像失败: " + e.getMessage());
         }
-
     }
 
     //修改密码
@@ -146,7 +151,21 @@ public class UserController {
         int likeCount = likeService.findUserLikeCount(userId);
         model.addAttribute("likeCount", likeCount);
 
+        // 关注数量
+        long followeeCount = followService.findFolloweeCount(userId, ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount", followeeCount);
+        // 粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER, userId);
+        model.addAttribute("followerCount", followerCount);
+        // 是否已关注
+        boolean hasFollowed = false;
+        if (hostHolder.getUser() != null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+        }
+        model.addAttribute("hasFollowed", hasFollowed);
+
         return "/site/profile";
     }
+
 
 }
